3f247c02157cc4f04c8363531fa07035ccaf45bc
Ravi <ravi.kumar1@assaabloy.com>
2024-09-27 20:49:54 +0530

    initial commit

---
+++ src/main/java/com/test/designPatterns/composite/Product.java
@@ -0,0 +1,17 @@
+package com.test.designPatterns.composite;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Product implements PriceAble {
+    private final String name;
+    private final double price;
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+}
