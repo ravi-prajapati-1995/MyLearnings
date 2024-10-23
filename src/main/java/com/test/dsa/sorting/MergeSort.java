4cbc9c8d6c09d9605fc6299d76766d4c2874ca4a
Ravi Kumar <ravi.kumar1@assaabloy.com>
2024-10-23 20:06:56 +0530

    merge sort

--- src/main/java/com/test/dsa/sorting/MergeSort.java
+++ src/main/java/com/test/dsa/sorting/MergeSort.java
@@ -7,17 +7,26 @@ import java.util.List;
 import static com.test.dsa.sorting.InsertionSort.swap;
 
 public class MergeSort {
-    /*
-        In merge sort we need to perform below steps:
-        1. Divide given array in two parts
-        2. Sort left part
-        3. Sort right part
-        4. Merge left and right
-     */
-    public static void main(String[] args) {
-        int[] arr = {24, 18, 38, 43, 14, 40, 1, 54}; //3, 5, 6, 9, 2, 11, 7
+	/**
+	* This algo is based on <b>Divide and merge
+	 * arr = [3, 5, 6, 9, 2, 11, 5], n=7
+	 * we will divide array into two parts
+	 * left part 4 : [3, 5, 6, 9] elements And right part 3: [2, 11, 5] elements
+	 * Take left part: [3, 5, 6, 9] and then divide it again into two part
+	 * left part: 2 elements: [3, 5] Right part 2 elements: [6, 9]
+	 * We will do this till we get only one element after that we will do same for right part
+	 * After this step we will merge both arrays like if we traverse left part then in last we will have [3] and [5]
+	 * For merge we will take list and two pointer 1st points to left start point and second points to right start point
+	 * we iterate this and compare elements and add in list
+	 * there may be case when left side there will be some element or may be in left side
+	 * Those elements will be the larger and we will add them in list directly
+	 * After that we will replace these values in array according to index we are merging array
+	* */
+	public static void main(String[] args) {
+		int arr[] = {1 ,3, 9, 7}; //3, 5, 6, 9, 2, 11, 7
+		int n = 4;
 
-        sortArray(arr, 0, arr.length - 1);
+        sortArray(arr, 0, n - 1);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -70,6 +79,11 @@ public class MergeSort {
             list.add(arr[right]);
             right++;
         }
+//		System.out.println(list);
+
+		for(int i=low; i<=high; i++) {
+			arr[i] = list.get(i - low);
+		}
 
         // adding back those value to original array
         for (int i = low; i <= high; i++) {
