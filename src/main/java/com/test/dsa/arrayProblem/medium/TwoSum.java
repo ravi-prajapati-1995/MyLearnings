d548dcc00b79b175d124cf640cc950f275675e72
Ravi Kumar <ravi.kumar1@assaabloy.com>
2024-11-20 19:20:02 +0530

    adding more questions

---
+++ src/main/java/com/test/dsa/arrayProblem/medium/TwoSum.java
@@ -0,0 +1,13 @@
+package com.test.dsa.arrayProblem.medium;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {};
+        twoSum(nums, 1);
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        return  null;
+    }
+}
