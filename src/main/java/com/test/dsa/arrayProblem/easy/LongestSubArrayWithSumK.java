02246433ad5728cd49b0e65b3d0b0a8f5e455750
Ravi Kumar <ravi.kumar1@assaabloy.com>
2024-11-20 19:20:02 +0530

    adding more questions

--- src/main/java/com/test/dsa/arrayProblem/easy/LongestSubArrayWithSumK.java
+++ src/main/java/com/test/dsa/arrayProblem/easy/LongestSubArrayWithSumK.java
@@ -1,9 +1,12 @@
 package com.test.dsa.arrayProblem.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithSumK {
     public static void main(String[] args) {
-        int arr[] = {1, -1, 5, -2, 3};
-        int i = lenOfLongestSubarr(arr, 3);
+        int arr[] = {1, 1, 1, 50, 1, 1, 1, 50, 2, 3, 2, 3};
+        int i = lenOfLongestSubArrTwoPointerApproach(arr, 6);
         System.out.println(i);
     }
 
@@ -23,5 +26,51 @@ public class LongestSubArrayWithSumK {
             }
         }
         return maxlen;
+    }
+
+    public static int lenOfLongestSubarrBetterSolutionValidForPositiveCase(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if(sum == k) {
+                maxLen = i+1;
+            }
+            int rem = sum - k;
+            if(map.containsKey(rem)) {
+                final var index = map.get(rem);
+                maxLen = Math.max(maxLen, i - index);
+            }
+
+            //we need to add element only when that sum is not existed previously
+            if(!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
+
+    public static int lenOfLongestSubArrTwoPointerApproach(int[] arr, int k) {
+        int maxLen = 0;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        while (i < arr.length) {
+            if(sum > k) {
+                sum = sum - arr[j];
+                j++;
+            } else {
+                if(sum == k) {
+                    maxLen = Math.max(i - j, maxLen);
+                }
+                sum += arr[i];
+                i++;
+
+            }
+
+        }
+
+        return maxLen;
     }
 }
