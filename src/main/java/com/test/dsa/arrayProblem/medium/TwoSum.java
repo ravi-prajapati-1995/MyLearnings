8b22b111e4c53660a669805a2ae8d91aed996332
Ravi Kumar <ravikumartholiya@gmail.com>
2024-11-21 09:09:21 +0530

    add two sum

--- src/main/java/com/test/dsa/arrayProblem/medium/TwoSum.java
+++ src/main/java/com/test/dsa/arrayProblem/medium/TwoSum.java
@@ -1,13 +1,28 @@
 package com.test.dsa.arrayProblem.medium;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] nums = {};
-        twoSum(nums, 1);
+        int[] nums = {3,3};
+        int[] ints = twoSum(nums, 6);
+        System.out.println(Arrays.toString(ints));
 
     }
     public static int[] twoSum(int[] nums, int target) {
-        return  null;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{0, 1};
     }
 }
