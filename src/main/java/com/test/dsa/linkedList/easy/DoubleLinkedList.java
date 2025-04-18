75473c34474e7275d1258fe831cf1e7e5146630a
Ravi Kumar <ravi.kumar1@assaabloy.com>
2025-04-18 11:58:56 +0530

    adding double linked list

--- src/main/java/com/test/dsa/linkedList/easy/DoubleLinkedList.java
+++ src/main/java/com/test/dsa/linkedList/easy/DoubleLinkedList.java
@@ -1,4 +1,43 @@
 package com.test.dsa.linkedList.easy;
 
 public class DoubleLinkedList {
+    public static void main(String[] args) {
+        final var ints = new int[]{1, 2, 3, 4, 5};
+        final var integerNode = Node.fromArray(ints);
+        Node.print(integerNode);
+
+    }
+}
+
+class Node<T> {
+    private Node prev;
+    private T data;
+    private Node next;
+
+    Node(final Node prev, final T data, final Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+
+    static Node<Integer> fromArray(int[] array) {
+        Node head = new Node(null, array[0], null);
+        Node prev = head;
+        Node curr = null;
+        for (int i = 1; i < array.length; i++) {
+            curr = new Node(prev, array[i], null);
+            prev.next = curr;
+
+            prev = curr;
+        }
+        return head;
+    }
+
+    static void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
 }
