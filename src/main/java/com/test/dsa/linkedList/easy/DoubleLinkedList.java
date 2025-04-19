5cdbb207b900050b46d4c8fa2d8a5b4528f06fa1
Ravi Kumar <ravikumartholiya@gmail.com>
2025-04-19 20:20:18 +0530

    Double linked list adding element to tail and head

--- src/main/java/com/test/dsa/linkedList/easy/DoubleLinkedList.java
+++ src/main/java/com/test/dsa/linkedList/easy/DoubleLinkedList.java
@@ -4,13 +4,17 @@ public class DoubleLinkedList {
     public static void main(String[] args) {
         final var ints = new int[]{1, 2, 3, 4, 5};
         final var integerNode = Node.fromArray(ints);
-        Node.print(integerNode);
+
+//        final var integerNode1 = Node.insertHead(integerNode, 22);
+        final var integerNode1 = Node.insertTail(integerNode, 232);
+
+        Node.print(integerNode1);
 
     }
 }
 
 /// ading comment test 1
-class Node<T> {
+ class Node<T> {
     private Node prev;
     private T data;
     private Node next;
@@ -19,6 +23,10 @@ class Node<T> {
         this.prev = prev;
         this.data = data;
         this.next = next;
+    }
+
+    Node(final T data) {
+        this.data = data;
     }
 
     static Node<Integer> fromArray(int[] array) {
@@ -31,6 +39,30 @@ class Node<T> {
 
             prev = curr;
         }
+        return head;
+    }
+
+    static <T> Node<T> insertHead(Node<T> head, T data) {
+       if(head == null) {
+           return new Node<>(data);
+       }
+
+        final var newNode = new Node<T>(null, data, null);
+        newNode.next = head;
+        return newNode;
+    }
+
+    static <T> Node<T> insertTail(Node<T> head, T data) {
+        if(head == null) {
+            return new Node<>(data);
+        }
+
+        var temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        final var newNode = new Node<T>(temp, data, null);
+        temp.next = newNode;
         return head;
     }
 
