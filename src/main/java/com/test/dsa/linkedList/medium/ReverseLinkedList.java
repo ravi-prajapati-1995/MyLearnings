c84c992a38cd64467e481dc755ddac1ed45324f4
Ravi Kumar <ravikumartholiya@gmail.com>
2025-04-27 12:52:20 +0530

    Reverse linked list using Recursion

--- src/main/java/com/test/dsa/linkedList/medium/ReverseLinkedList.java
+++ src/main/java/com/test/dsa/linkedList/medium/ReverseLinkedList.java
@@ -9,25 +9,25 @@ public class ReverseLinkedList {
     public static void main(String[] args) {
         final var integers = List.of(3, 2, 1, 0, -1, -2, 8);
         final var head = ListNode.from(integers);
-        final var listNode = reverseUsingPointers(head);
+//        final var listNode = reverseUsingPointers(head);
+        final var listNode = reverseRecursion(head);
         ListNode.print(listNode);
     }
 
     /**
      * Using stack we can reverse a linked list
-     * */
+     */
     public static ListNode reverseList(ListNode head) {
         final var list = new LinkedList<Integer>();
 
         ListNode temp = head;
-        while(temp != null ) {
+        while (temp != null) {
             list.push(temp.val);
             temp = temp.next;
         }
 
-
         temp = head;
-        while(temp != null ) {
+        while (temp != null) {
             temp.val = list.poll();
             temp = temp.next;
         }
@@ -37,10 +37,10 @@ public class ReverseLinkedList {
 
     /**
      * Here we will change the pointer we are pointing current next to the previous for all the elements
-     *
+     * <p>
      * TC - o(n)
      * SC - O(1)
-     * */
+     */
     public static ListNode reverseUsingPointers(ListNode head) {
 
         ListNode temp = head;
@@ -53,7 +53,31 @@ public class ReverseLinkedList {
             curr.next = prev;
             prev = curr;
         }
-        return  curr;
+        return curr;
     }
 
+    /**
+     * We can do it using recursion if we have
+     * 1 2 3 4 5
+     * we call for 2 3 4 5 and  3 4 5 so on till head.next can't be null
+     * After that we make link 5.next = 4 and 4.next = null
+     * and return newHead which will be 5 -> 4
+     * then in recursion head -> 3, newHead = 5, 4
+     *
+     * In this way we can reverse it
+     *
+     * <a href="https://takeuforward.org/data-structure/reverse-a-linked-list/">SOlution Article</a>
+     * [TODO] Revisit it after recursion completion
+     * */
+    public static ListNode reverseRecursion(ListNode head) {
+        if(head == null || head.next == null) {
+             return head;
+        }
+
+        ListNode newHead = reverseRecursion(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
 }
