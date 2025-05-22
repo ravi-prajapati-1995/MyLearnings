68a93262a8d4412307d545e4e3326c831fb45db3
Ravi Kumar <ravikumartholiya@gmail.com>
2025-05-22 19:13:22 +0530

    adding topping in pizza
    Say Happy Birthday to my Daughter, Kavya :)

--- src/main/java/com/test/systemdesign/designpatterns/structural/decorator/pizzashop/PizzaDecorator.java
+++ src/main/java/com/test/systemdesign/designpatterns/structural/decorator/pizzashop/PizzaDecorator.java
@@ -1,9 +1,10 @@
 package com.test.systemdesign.designpatterns.structural.decorator.pizzashop;
 
-abstract  public class PizzaDecorator {
+abstract public class PizzaDecorator extends BasePizza {
     final private BasePizza basePizza;
 
-    public PizzaDecorator(final BasePizza basePizza) {
+    protected PizzaDecorator(final BasePizza basePizza) {
+        super(basePizza.getSize(), basePizza.getDescription());
         this.basePizza = basePizza;
     }
 }
--- src/main/java/com/test/systemdesign/designpatterns/structural/decorator/pizzashop/toppings/VegTopping.java
+++ src/main/java/com/test/systemdesign/designpatterns/structural/decorator/pizzashop/toppings/VegTopping.java
@@ -9,5 +9,15 @@ public class VegTopping extends PizzaDecorator {
         super(basePizza);
     }
 
+    @Override
+    protected int price() {
+        int topping = switch (size) {
+            case REGULAR -> 30;
+            case MEDIUM -> 40;
+            case LARGE -> 50;
+            case EXTRA_LARGE -> 60;
+        };
 
+        return price() + topping;
+    }
 }
