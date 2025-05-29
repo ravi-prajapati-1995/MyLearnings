f9f0a339c292a67a629787248aadbaa561b337f6
Ravi Kumar <ravikumartholiya@gmail.com>
2025-05-29 09:30:49 +0530

    Creating a binary tree from given list

--- src/main/java/com/test/dsa/binartree/TreeNode.java
+++ src/main/java/com/test/dsa/binartree/TreeNode.java
@@ -23,6 +23,23 @@ public class TreeNode {
         return root;
     }
 
+    /**
+     * While inserting any element in BST
+     *
+     *        4
+     *      /   \
+     *     2     6
+     *    / \   / \
+     *   1   3 5   7
+     *
+     * Suppose we have above BST and need to insert 8
+     * 1. Start with root check if coming value is greater than root.val if yes
+     * 2. Move Right, then check if right is not null, if yes create a new node and point temp.right to that
+     * 3. if right not null then move till we found null
+     * 4. If current val is less than val then move left and check left and val again in same way
+     * 5. When we found null value then insert current node there
+     *
+     * */
     public static TreeNode insertIntoBST(TreeNode root, int val) {
         if (root == null) {
             return new TreeNode(val);
