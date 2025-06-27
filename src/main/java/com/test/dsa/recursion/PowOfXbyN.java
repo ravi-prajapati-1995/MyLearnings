27d5fadfd2b282da99d038a904adf25734f277b6
Ravi Kumar <ravikumartholiya@gmail.com>
2025-06-27 20:41:36 +0530

    power of x by N brute force solution

--- src/main/java/com/test/dsa/recursion/PowOfXbyN.java
+++ src/main/java/com/test/dsa/recursion/PowOfXbyN.java
@@ -7,6 +7,32 @@ public class PowOfXbyN {
         System.out.println(v);
     }
 
+    /**
+     *
+     * Using Iterative approach
+     * 1. Simple multiplying x , n times in for loop
+     * 2. In case of number is negative update x to 1/x and multiply n *-1 so that it can converted to positive number
+     * 3. return the result
+     * @param x -- Number for which we required power
+     * @param n --- How many time number should be multiply can be in negative
+     * @return
+     * TC - O(n) we are using loop
+     * SC - O(1) as we are not using any extra space
+     */
+    public static double myPowBruteForce(double x, int n) {
+        double res = 1;
+        if(n < 0) {
+            n = n * -1;
+            x = 1/x;
+        }
+
+        for (int i = 0; i < n; i++) {
+            res = res * x;
+        }
+        return res;
+    }
+
+
     public static double myPow(double x, int n) {
         if(n == 0) {
             return x;
@@ -14,7 +40,7 @@ public class PowOfXbyN {
             return myPowPositive(x, n);
         } else {
             final var i = ~n + 1;
-            final var v = myPowPositive(x, i);
+            final var v = myPowPositive(1/x, i);
             return 1 / v;
         }
     }
