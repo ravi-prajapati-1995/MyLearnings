227c2b0c378f317a124cd29e100134477a18b6f7
Ravi Kumar <ravikumartholiya@gmail.com>
2025-06-27 19:56:27 +0530

    Atoi using recursion

--- src/main/java/com/test/dsa/recursion/StringToInteger_Atoi.java
+++ src/main/java/com/test/dsa/recursion/StringToInteger_Atoi.java
@@ -3,9 +3,9 @@ package com.test.dsa.recursion;
 /**
  * <a href="https://leetcode.com/problems/string-to-integer-atoi/">Problem Link</a>
  * Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
- *
+ * <p>
  * The algorithm for myAtoi(string s) is as follows:
- *
+ * <p>
  * Whitespace: Ignore any leading whitespace (" ").
  * Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
  * Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or
@@ -13,24 +13,73 @@ package com.test.dsa.recursion;
  * Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range.
  * Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
  * Return the integer as the final result.
- * */
+ */
 public class StringToInteger_Atoi {
 
     public static void main(String[] args) {
 //        final var myAtoi = atoi("   -042");
 //        System.out.println(myAtoi);
-        final var atoi = getAtoi("42".toCharArray(), 0);
+        final var atoi = atoi("000000000000000000");
         System.out.println(atoi);
     }
 
+    private static int getAtoi(final char[] charArray, final int i, long result, int sign) {
 
+        if (i == charArray.length) {
+            return (int)result;
+        }
+
+        final var ch = charArray[i];
+        if (ch >= 48 && ch <= 57) {
+            int number = ch - '0';
+            result = result * 10L + number;
+
+            if ((result * sign) >= Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if ((result * sign) <= Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            return getAtoi(charArray, i + 1, result, sign);
+        } else {
+            return  (int)result;
+        }
+    }
+
+    public static int atoi(String s) {
+        if (s.isBlank()) {
+            return 0;
+        }
+        final var charArray = s.toCharArray();
+        int sign = 1;
+        int idx = 0;
+        while (charArray[idx] == ' ') {
+            idx++;
+        }
+
+        if (charArray[idx] == '-') {
+            sign = -1;
+            idx++;
+        } else if (charArray[idx] == '+') {
+            idx++;
+        }
+
+        while ( idx < charArray.length && charArray[idx] == '0') {
+            idx++;
+        }
+
+        System.out.println(idx + "\t" + s);
+        System.out.println(s.substring(idx));
+        return getAtoi(charArray, idx, 0, sign);
+    }
 
     public static int myAtoiIterative(String s) {
 
         long result = 0;
         int sign = 1;
         s = s.trim(); // trim leading
-        if(s.isEmpty()) return 0;
+        if (s.isEmpty()) return 0;
 
         final var _1stChar = s.charAt(0);
         if (_1stChar == '-') {
@@ -55,48 +104,5 @@ public class StringToInteger_Atoi {
         }
 
         return (int) result * sign;
-    }
-
-    public static int atoi(String s) {
-        if(s.isBlank()) {
-            return 0;
-        }
-
-        final var charArray = s.toCharArray();
-        int sign = 1;
-        int idx = 0;
-        while(charArray[idx] == ' ') {
-            idx++;
-        }
-        if(charArray[idx] == '-') {
-            sign = -1;
-            idx++;
-        } else if(charArray[idx] == '+') {
-            idx++;
-        }
-
-        while(charArray[idx] == '0') {
-            idx++;
-        }
-
-        System.out.println(idx + "\t" +s);
-        System.out.println(s.substring(idx));
-        return 0;
-    }
-
-    private static int getAtoi(final char[] charArray, final int i) {
-        if(i == charArray.length) {
-            return 0;
-        }
-        final var ch = charArray[i];
-        if(ch >= 48 && ch <= 57) {
-            int number = Integer.parseInt(ch + "");
-            System.out.println(number+"-----------------------------");
-            final var res = number * 10 + getAtoi(charArray, i + 1);
-            System.out.println(res);
-            return res;
-        } else {
-            return 0;
-        }
     }
 }
