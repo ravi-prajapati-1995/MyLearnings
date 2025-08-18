3724141e6424e7eea5ecc28b2509f50b80a96f4c
Ravi Kumar <ravikumartholiya@gmail.com>
2025-08-18 09:31:46 +0530

    adding comments

--- src/main/java/com/ravi/learnings/dsa/bit_manipulation/easy/DecimalToBinary.java
+++ src/main/java/com/ravi/learnings/dsa/bit_manipulation/easy/DecimalToBinary.java
@@ -4,7 +4,7 @@ import lombok.val;
 
 public class DecimalToBinary {
     public static void main(String[] args) {
-        final var decimalToBinary = decimalToBinary(14);
+        final var decimalToBinary = decimalToBinary(1501);
         System.out.println(decimalToBinary);
         System.out.println(binaryToDecimal(decimalToBinary));
 //        decimalToBinary2(14);
@@ -14,7 +14,8 @@ public class DecimalToBinary {
     /**
      * 1/2 = 0
      * 1. Take a while loop till we have Quotient is greater than 0 take string builder and append reminder
-     * 2.Then store remainder s
+     * 2.Then store remainder in string builder
+     * 3. after completion of the loop we will reverse the string
      *
      */
     public static String decimalToBinary(int val) {
@@ -31,6 +32,8 @@ public class DecimalToBinary {
      * str = "10101"
      * We start from the right to left and start multiplying with 2^0 , 2^1, 2^2... so on till we reaches at the end
      * Take a sum variable and increase it in while loop
+     * For each iteration multiply _2pow with 2 as power of 2 getting increase from right to left
+     * 
      * */
     public static int binaryToDecimal(String bs) {
         var charArray = bs.toCharArray();
---
+++ src/main/java/com/ravi/learnings/dsa/recursion/hard/MColoringProblem.java
@@ -0,0 +1,10 @@
+package com.ravi.learnings.dsa.recursion.hard;
+
+/**
+ * <a href="https://www.geeksforgeeks.org/problems/m-coloring-problem-1587115620/1">Problem Link</a>
+ * You are given an undirected graph consisting of V vertices and E edges represented by a list edges[][], along with an integer m.
+ * Your task is to determine whether it is possible to color the graph using at most m different colors such that no two
+ * adjacent vertices share the same color. Return true if the graph can be colored with at most m colors, otherwise return false.
+ * */
+public class MColoringProblem {
+}
