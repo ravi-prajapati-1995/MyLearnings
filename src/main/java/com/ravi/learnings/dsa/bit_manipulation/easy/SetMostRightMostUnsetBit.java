a0f47158bab46d1aff991d8cdf4109521d9cc12e
Ravi Kumar <ravikumartholiya@gmail.com>
2025-09-01 09:31:15 +0530

    some more binary problems

--- src/main/java/com/ravi/learnings/dsa/bit_manipulation/easy/SetMostRightMostUnsetBit.java
+++ src/main/java/com/ravi/learnings/dsa/bit_manipulation/easy/SetMostRightMostUnsetBit.java
@@ -12,9 +12,24 @@ package com.ravi.learnings.dsa.bit_manipulation.easy;
 public class SetMostRightMostUnsetBit {
     public static void main(String[] args) {
         System.out.println(setMostRightMostUnsetBit(9));
+        System.out.println(DecimalToBinary.decimalToBinary(49972));
     }
     public static int setMostRightMostUnsetBit(final int n) {
-
-        return 0;
+        /**
+         * 6 = 110
+         * 7 = 111
+         * --------
+         * 86 = 1010110
+         * 87 = 1010111
+         *
+         * ---------------------
+         * 49971 = 1100001100110011
+         * 49972 = 1100001100110100
+         *
+         * From above observation if we add one in the element of it will set the the rightmost unset bit
+         * So we do OR(|) with the current number then we can set the most rightmost bit of the number
+         *
+         */
+        return n | n +1;
     }
 }
---
+++ src/main/java/com/ravi/learnings/dsa/bit_manipulation/easy/SwapNumberUsingBitOperators.java
@@ -0,0 +1,17 @@
+package com.ravi.learnings.dsa.bit_manipulation.easy;
+
+public class SwapNumberUsingBitOperators {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * when we do 5 ^ 5 = 0
+     * lets take example a = 4 (100) and b = 6(110)
+     *
+     * 4 ^ 4 = 100 ^ 100
+     * */
+    public static void swap(int a, int b) {
+
+    }
+}
