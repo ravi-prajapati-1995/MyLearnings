7d0c5818ac37d106013b9a548030286eef38b5b1
Ravi Kumar <ravikumartholiya@gmail.com>
2025-09-20 17:47:32 +0530

    formatting changes

---
+++ src/main/java/com/ravi/learnings/dsa/stack/learning/StackUsingQueue.java
@@ -0,0 +1,27 @@
+package com.ravi.learnings.dsa.stack.learning;
+
+/**
+ * I am having stack DS and need to use it as Queue
+ * */
+public class StackUsingQueue {
+
+    public static void main(String[] args) {
+
+    }
+
+
+  /*  public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public boolean isEmpty() {
+    }*/
+}
