cade42574ac38fe380e3fc0b78c3b190f040d8f1
Ravi Kumar <ravikumartholiya@gmail.com>
2025-09-22 20:30:22 +0530

    No work

--- src/main/java/com/ravi/learnings/dsa/stack/learning/StackUsingQueue.java
+++ src/main/java/com/ravi/learnings/dsa/stack/learning/StackUsingQueue.java
@@ -1,7 +1,13 @@
 package com.ravi.learnings.dsa.stack.learning;
 
 /**
- * I am having stack DS and need to use it as Queue
+ * I am having Queue DS and need to use it as Stack
+ * in stack Last-In-First-Out and in Queue First-In-First-Out
+ * So I will have some elements and will push it into queue and lets see it work as queue or not
+ * st.push(5)
+ * st.top -- It will give 5
+ * st.push(3)
+ * st.top() --- Now as it is queue it will give us 5 so we will add
  * */
 public class StackUsingQueue {
 
