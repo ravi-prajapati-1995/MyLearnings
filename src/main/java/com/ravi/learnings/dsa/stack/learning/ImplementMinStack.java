c991892498013099c6b904b1ba8128291d507ff2
Ravi Kumar <ravikumartholiya@gmail.com>
2025-09-28 18:25:14 +0530

    Some test case breaks need to fix

--- src/main/java/com/ravi/learnings/dsa/stack/learning/ImplementMinStack.java
+++ src/main/java/com/ravi/learnings/dsa/stack/learning/ImplementMinStack.java
@@ -1,5 +1,7 @@
 package com.ravi.learnings.dsa.stack.learning;
 
+import lombok.val;
+
 import javax.print.DocFlavor.INPUT_STREAM;
 import java.util.*;
 import java.util.AbstractMap.SimpleEntry;
@@ -13,24 +15,81 @@ import java.util.Map.Entry;
  * */
 public class ImplementMinStack {
     public static void main(String[] args) {
-        final var minStack = new MinStack();
-        minStack.push(-10);
-        minStack.push(14);
+        final var minStack = new MinStackOptimal();
+        minStack.push(5);
+        minStack.push(6);
         System.out.println(minStack.getMin());
-        System.out.println(minStack.getMin());
-        minStack.push(-20);
-        System.out.println(minStack.getMin());
-        System.out.println(minStack.top());
+        minStack.push(4);
         System.out.println(minStack.getMin());
         minStack.pop();
-
-        minStack.push(10);
-        minStack.push(-7);
-
         System.out.println(minStack.getMin());
-
+        System.out.println(minStack.top());
 
     }
+}
+
+/**
+ * In brute force approach we are using space 2N
+ * To optimize that we will use the formula
+ * 1. If any element which we are inserting is smaller than the minValue then we will update the current value
+ * and update the current minimum
+ * newValue = 2*value - min
+ * lets take example minValue = MIN_VALUE, st = stack
+ * st.push(4) --- minValue = 4
+ * st.push(5) --- minValue = 4
+ * st.push(3) --- So when pushing 3 we will change this to val = 2*3-4 = 2 so we will store or push 2
+ * and replace minValue to 3
+ * Formula is: newVal = 2 * val - previousMin
+ * To get prevMin = 2 * val - newVal
+ * When we pop and check if the current number is less than the minVal then it is modified number we need to
+ * revert it
+ *
+ * */
+class MinStackOptimal {
+    private  final LinkedList<Integer> stack;
+    private int minVal;
+    public MinStackOptimal() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        if(stack.isEmpty()) {
+            minVal = val;
+            stack.push(val);
+            return;
+        }
+
+       if(val < minVal) {
+           int newVal = (2 * val) - minVal; // Need to remember this formula
+           stack.push(newVal);
+           minVal = val;
+       } else {
+           stack.push(val);
+       }
+    }
+
+    /*
+    * While pop element
+    * */
+    public void pop() {
+        final var pop = stack.pop();
+        if(pop < minVal) {
+            minVal = 2 * minVal - pop;
+        }
+    }
+
+    public int top() {
+        final var peek = stack.peek();
+        if(peek < minVal) { //Then it is modified value we need to revert it
+            return minVal;
+        }
+        return peek;
+    }
+
+    public int getMin() {
+        return minVal;
+    }
+
 }
 
 /**
@@ -38,6 +97,9 @@ public class ImplementMinStack {
  * that is pushed to the stack and second is the min element that we have seen till now
  * In push operation are checking if stack is already empty then directly push to the stack
  * or else push val and min value of current or top element
+ *
+ * TC = O(1) -- Doing all operations in one go
+ * SC = O(2*N) -- Storing value and min value in pair
  * */
 class MinStack {
     private  final LinkedList<Entry> stack;
