83800cd48ee9e1bba8c0b7ddc5bf220ebf261003
Ravi Kumar <ravikumartholiya@gmail.com>
2025-09-29 18:06:34 +0530

    Starting

---
+++ src/main/java/com/ravi/learnings/dsa/stack/prefix_infix_postfix/InfixToPostfix.java
@@ -0,0 +1,80 @@
+package com.ravi.learnings.dsa.stack.prefix_infix_postfix;
+
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * You are given a string s representing an infix expression. Convert this infix expression to a postfix expression.
+ * <p>
+ * Infix expression: The expression of the form a op b. When an operator is in between every pair of operands.
+ * Postfix expression: The expression of the form a b op. When an operator is followed for every pair of operands.
+ * <p>
+ * Input: a*(b+c)/d
+ * output: abc+*d/
+ * Operator and priority
+ * ^ ------- 3
+ * * / ------- 2
+ * + - ------- 1
+ */
+public class InfixToPostfix {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * So to convert infix to postfix we will do the following steps:
+     * I will traverse character by character when and check if it is operator or operand
+     * if I get operator then I will put it in stack
+     * If I get operand I will append it in result String
+     * If while inserting in stack there is already operand available then we will check priority
+     * If it has priority greater than what on the top then simply add it
+     * Else we will pop out elements from stack and add to result till we find operator that has lower prirority
+     * than this sign
+     * If we see opening bracket directly
+     */
+    public static String infixToPostfix(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder result = new StringBuilder(s.length());
+        for (final char ch : s.toCharArray()) {
+            if (isOperand(ch)) {
+                result.append(ch);
+            } else {
+                if (canAddOperator(ch, st)) {
+                    st.push(ch);
+                } else {
+                    // Suppose here * is coming and we have )
+                    while(!st.isEmpty() && priority(ch) <= priority(st.peek())) {
+
+                    }
+
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    private static boolean canAddOperator(final char ch, final Stack<Character> st) {
+        if (st.isEmpty()) {
+            return true;
+        }
+        final var peek = st.peek();
+        return priority(ch) >= priority(peek);
+    }
+
+    private static int priority(final char ch) {
+        if (ch == '^') {
+            return 3;
+        } else if (ch == '/' || ch == '*') {
+            return 2;
+        } else if (ch == '+' || ch == '-') {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private static boolean isOperand(final char ch) {
+        final var characters = List.of('+', '-', '*', '(', '^', ')');
+        return !characters.contains(ch);
+    }
+}
