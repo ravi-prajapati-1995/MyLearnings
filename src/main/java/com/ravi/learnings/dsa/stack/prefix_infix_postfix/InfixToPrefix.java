0bf9174c6cbced594a0c333104174d5844bce2e4
Ravi Kumar <ravikumartholiya@gmail.com>
2025-10-04 09:52:59 +0530

    infix to prefix

--- src/main/java/com/ravi/learnings/dsa/stack/prefix_infix_postfix/InfixToPrefix.java
+++ src/main/java/com/ravi/learnings/dsa/stack/prefix_infix_postfix/InfixToPrefix.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * Video Link: https://youtu.be/4pIc9UBHJtk?si=pTxB3cva1rFXFzMw&t=1001
+ * Video Link: <a href="https://youtu.be/4pIc9UBHJtk?si=pTxB3cva1rFXFzMw&t=1001">Video Link</a>
  * To change InfixToPrefix we will follow the following steps:
  * 1. Reverse the Infix
  * 2. Infix 2 Postfix -- With conditional changes
@@ -16,40 +16,84 @@ import java.util.Stack;
  */
 public class InfixToPrefix {
     public static void main(String[] args) {
-        System.out.println(infixToPrefix("ravi"));
+        System.out.println(infixToPrefix("a+b*(c^d-e)^(f+g*h)-i"));
     }
 
     public static String infixToPrefix(String s) {
         // Reversed the given String
+        var reversed = getReversed(s);
+        char[] charArray;
+
+        System.out.println(reversed);
+        final var string = reversed.toString();
+        // Replacing ( with ) and ) with (
+        final var replace = string.replace("(", "#").replace(")", "@");
+        final var replace1 = replace.replace("#", ")").replace("@", "(");
+        System.out.println(replace1);
+
+        Stack<Character> st = new Stack<>();
+        charArray = replace1.toCharArray();
+        StringBuilder res = new StringBuilder();
+        for(char ch: charArray) {
+            if(isOperand(ch)) {
+                res.append(ch);
+            } else {
+                if(ch == ')'){
+                    while (!st.isEmpty() && st.peek() != '(') {
+                        res.append(st.pop());
+                    }
+                    st.pop();
+                } else if(ch == '^') { // In case of ^ we will get all the operator which has less priority than
+                    // this: Control conversion
+                    while(!st.isEmpty() && priority(ch) <= priority(st.peek())) {
+                        res.append(st.pop());
+                    }
+
+                    st.push(ch);
+                } else if(ch == '(') {
+                    st.push(ch);
+                } else { // When I got operator other than ^ or got opening bracket
+                    while(!st.isEmpty() && priority(ch) < priority(st.peek())) {
+                        res.append(st.pop());
+                    }
+
+                    st.push(ch);
+                }
+            }
+        }
+
+        while (!st.isEmpty()) {
+            res.append(st.pop());
+        }
+
+        return getReversed(res.toString()).toString();
+    }
+
+    private static StringBuilder getReversed(final String s) {
         StringBuilder reversed = new StringBuilder();
         int size = s.length();
         char[] charArray = s.toCharArray();
         for (int i = size - 1; i >= 0; i--) {
             reversed.append(charArray[i]);
         }
-
-        System.out.println(reversed);
-        Stack<Character> st = new Stack<>();
-        final var string = reversed.toString();
-//        string.replace("(", ")").replace();
-        charArray = string.toCharArray();
-        StringBuilder res = new StringBuilder();
-        for(char ch: charArray) {
-            if(isOperand(ch)) {
-                res.append(ch);
-            } else {
-                if(ch == '('){
-
-                }
-            }
-        }
-
-        return null;
+        return reversed;
     }
 
     private static boolean isOperand(final char ch) {
         final var characters = List.of('+', '-', '*', '(', '^', ')', '/');
         return !characters.contains(ch);
+    }
+
+    private static int priority(final char ch) {
+        if (ch == '^') {
+            return 3;
+        } else if (ch == '/' || ch == '*') {
+            return 2;
+        } else if (ch == '+' || ch == '-') {
+            return 1;
+        }
+
+        return 0;
     }
 
 }
