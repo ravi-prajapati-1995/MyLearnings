d19c257af44449681be486efb30ba205c3822488
Ravi Kumar <ravikumartholiya@gmail.com>
2025-10-17 19:11:45 +0530

    trapping water brute force

--- src/main/java/com/ravi/learnings/dsa/stack/monotonic_stack/TappingWater.java
+++
@@ -1,4 +0,0 @@
-package com.ravi.learnings.dsa.stack.monotonic_stack;
-
-public class TappingWater {
-}
---
+++ src/main/java/com/ravi/learnings/dsa/stack/monotonic_stack/TrappingWater.java
@@ -0,0 +1,67 @@
+package com.ravi.learnings.dsa.stack.monotonic_stack;
+
+/**
+ * <a href="https://leetcode.com/problems/trapping-rain-water/description/">Problem Link</a>
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it can trap after raining.
+ */
+public class TrappingWater {
+
+    public static void main(String[] args) {
+        int[] height = {4,2,0,3,2,5};
+        System.out.println(trap(height));
+    }
+
+    /**
+     * To store the water in the building there should be taller building in the left and taller in the right
+     * And how much water we can store between these taller building is the min(leftMax, rightMax)
+     * Now for calculate the water we will decrease the current building hight from left and right taller building
+     * so formula will be: min(leftMax, rightMax) - arr[i], where i is the current building
+     * Keep in mind if both the left max and right max are greater than arr[i] in that case only
+     * if I have 3 building and all have 2 height then water will not be stored
+     *
+     * TC - O(N) (suffixmax) + O(N) (prefix max) + O(n) to get the trap water  == O(3N)
+     * SC - O(3N) --> O(N) -- For get prefix max + O(N) get suffix Max + O(N) to return the result
+     */
+    public static int trap(int[] height) {
+        // To store the total water
+        int result = 0;
+        int[] prefixMax = getPrefixMax(height);
+        int[] suffixMax = getSuffixMax(height);
+
+        for (int i = 0; i < height.length; i++) {
+
+            //water will be stored only if current element is less than from left and right building
+            if (height[i] < prefixMax[i] && height[i] < suffixMax[i]) {
+                result = result + Math.min(prefixMax[i], suffixMax[i]) - height[i];
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * So this will help us to find out building max building size in the left from the current element
+     */
+    private static int[] getPrefixMax(final int[] height) {
+        int[] prefix = new int[height.length];
+        prefix[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            prefix[i] = Math.max(prefix[i - 1], height[i]);
+        }
+        return prefix;
+    }
+
+    /**
+     * This will helps us to find the taller tower in the right side of the current element
+     */
+    private static int[] getSuffixMax(final int[] height) {
+        final var length = height.length;
+        int[] suffix = new int[length];
+        suffix[length - 1] = height[length - 1];
+        for (int i = length - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], height[i]);
+        }
+        return suffix;
+    }
+}
