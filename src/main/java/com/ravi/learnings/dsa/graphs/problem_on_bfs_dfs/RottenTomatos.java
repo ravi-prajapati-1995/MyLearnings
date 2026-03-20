797a29260ae81094ca2dd6e05f37ea1f402cc8f2
Ravi Kumar <ravikumartholiya@gmail.com>
2026-03-20 10:53:12 +0530

    Rotten tomatos

--- src/main/java/com/ravi/learnings/dsa/graphs/problem_on_bfs_dfs/RottenTomatos.java
+++ src/main/java/com/ravi/learnings/dsa/graphs/problem_on_bfs_dfs/RottenTomatos.java
@@ -2,6 +2,7 @@ package com.ravi.learnings.dsa.graphs.problem_on_bfs_dfs;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Queue;
 
 /**
@@ -25,29 +26,75 @@ import java.util.Queue;
 public class RottenTomatos {
     public static void main(String[] args) {
         int[][] arr = {
-                {2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1},
-                {1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0},
-                {1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1},
-                {0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1},
-                {1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2},
-                {1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1},
-                {0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1},
-                {1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0},
-                {1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1},
-                {2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1},
-                {1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0},
-                {1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1},
-                {0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1},
-                {1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2},
-                {1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1},
-                {0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1},
-                {1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0},
-                {1,1,2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1},
-                {2,1,1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1},
-                {1,0,1,1,0,1,1,2,1,1,0,1,1,0,1,1,2,1,1,0}
+                { 2, 1, 1 },
+                { 1, 1, 0 },
+                { 0, 1, 1 }
         };
 
-        orangesRotting(arr);
+        orangesRottingStriverWay(arr);
+    }
+
+    /**
+     * if (row, col) represents the then
+     * row-1, col --- represents the orange of upside
+     * row + 1, col --- represents the down orange
+     * row, col-1 ----- for Left side orange
+     * row, col + 1 -- for right side orange
+     * So if we create a array of [Up, right, down left]
+     * delRow = [-1, 0, 1, 0]
+     * delCol = [0, 1, 0, -1]
+     *
+     * For any row, col by adding delRow and delCol we can get the all four side of
+     * oranges
+     * 
+     * @param grid
+     * @return
+     */
+    public static int orangesRottingStriverWay(int[][] grid) {
+        int[] delRow = { -1, 0, 1, 0 };
+        int[] delCol = { 0, 1, 0, -1 };
+        Queue<Pair> queue = new ArrayDeque<>();
+        int rows = grid.length;
+        int columns = grid[0].length;
+        int freshTomato = 0;
+        // Get the already rotten tomatoes in the queue
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new Pair(i, j, 0));
+                } else if (grid[i][j] == 1) {
+                    freshTomato++;
+                }
+            }
+        }
+
+        int count = 0;
+        int minutes = 0;
+
+        while (!queue.isEmpty()) {
+            final var poll = queue.poll();
+            minutes = Math.max(minutes, poll.tm);
+
+            // Getting the four neighbours of current
+            for (int i = 0; i < 4; i++) {
+                int currRow = poll.i + delRow[i];
+                int currCol = poll.j + delCol[i];
+
+                // Checking if we get the valid row and col
+                if (currRow >= 0 && currRow < rows && currCol >= 0 && currCol < columns
+                        && grid[currRow][currCol] == 1) {
+                    // Marking current row rotten
+                    grid[currRow][currCol] = 2;
+                    count++;
+                    queue.add(new Pair(currRow, currCol, poll.tm + 1));
+                }
+            }
+
+        }
+
+        System.out.println(Arrays.deepToString(grid));
+        System.out.println(freshTomato + "\t" + count + "\t" + minutes);
+        return freshTomato != count ? -1 : 0;
     }
 
     /**
@@ -58,7 +105,9 @@ public class RottenTomatos {
      * 3. After marking all orange rotten we will add each of them in queue with
      * time unit
      * 4. We will repeat the process till queue is not empty
-     *
+     * 
+     * TC = N * M
+     * SC = N * M
      * @param grid
      * @return
      */
@@ -111,7 +160,7 @@ public class RottenTomatos {
         }
 
         System.out.println(Arrays.deepToString(grid));
-        System.out.println(freshTomato +"\t" + count +"\t" + minutes);
+        System.out.println(freshTomato + "\t" + count + "\t" + minutes);
         return freshTomato != count ? -1 : 0;
     }
 
@@ -131,5 +180,16 @@ public class RottenTomatos {
         return poll.i - 1 >= 0 && grid[poll.i - 1][poll.j] == 1;
     }
 
-    public record Pair(int i, int j, int tm) {}
+    public static final class Pair {
+        public final int i;
+        public final int j;
+        public final int tm;
+
+        public Pair(int i, int j, int tm) {
+            this.i = i;
+            this.j = j;
+            this.tm = tm;
+        }
+
+    }
 }
